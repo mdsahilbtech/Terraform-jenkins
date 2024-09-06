58d9eef92b2ce1a08f96f9848ba7aae97fe1730a
mdsahilbtech <150688998+mdsahilbtech@users.noreply.github.com>
2024-09-06 15:28:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    region = "us-east-1"  
+shared_credentials_files = ["~/.aws/credentials"]
+region = "us-east-1"
 }
-
 resource "aws_instance" "foo" {
   ami           = "ami-070f589e4b4a3fece" # us-west-2
   instance_type = "t2.micro"
